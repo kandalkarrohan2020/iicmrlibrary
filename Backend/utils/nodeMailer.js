f5f9390c5a6b4268dced318b1c20e5210f36f86f
kandalkarrohan2020 <kandalkarrohan2020@gmail.com>
2025-11-24 18:18:06 +0530

    Changed Email Pass And SMTP SERVER

--- Backend/utils/nodeMailer.js
+++ Backend/utils/nodeMailer.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport(
     secure: false,
     auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.BREVO_SMTP_KEY,
+    pass: process.env.EMAIL_PASS,
   },
   },
   {
