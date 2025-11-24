46d8263a15275798efad12a4ff6a103a8699ac94
kandalkarrohan2020 <kandalkarrohan2020@gmail.com>
2025-11-24 19:26:04 +0530

    Checking Complete

--- Backend/utils/nodeMailer.js
+++ Backend/utils/nodeMailer.js
@@ -3,21 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport(
-  {
-    host: process.env.EMAIL_HOST,
-    port: 587,
-    secure: false,
-    auth: {
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: 587,
+  secure: false,
+  auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  },
-  {
-    debug: true,
-    logger: true,
-  }
-);
+});
 
 const sendEmail = async (email, username, password, role, url) => {
   try {
